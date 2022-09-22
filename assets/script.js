8f429c91403b81ac6015e826beffec5290db4c06
Kaybe111 <keith.byrne01@gmail.com>
2022-09-22 19:06:12 +0000

    Add JS code to articulate boxng glove  cursor for mousedown and mouseup

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,25 @@
 const cursor = document.getElementById('cursor');
 
+/**
+ * This function makes the boxing glove move with the cursor
+ */
 window.addEventListener('mousemove', function(event) {
     cursor.style.top = event.pageY + 'px';
     cursor.style.left = event.pageX + 'px';
+})
+
+/**
+ * This function makes the glove rotate on mousedown
+ * to simulate giving a punch
+ */
+window.addEventListener('mousedown', function() {
+    cursor.style.transform = 'translate(-50%, -20%) rotate(70deg)';
+})
+
+/**
+ * This function returns the boxing glove to its
+ * rest state
+ */
+ window.addEventListener('mouseup', function() {
+    cursor.style.transform = 'translate(-50%, -20%)';
 })
