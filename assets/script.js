75e484302d7ea51daa87360ba2b14833bc86724f
Kaybe111 <keith.byrne01@gmail.com>
2022-09-23 09:16:13 +0000

    Add timer to remove character tile from hole after 1500 milliseconds

--- assets/script.js
+++ assets/script.js
@@ -24,6 +24,10 @@ window.addEventListener('mouseup', function () {
     cursor.style.transform = 'translate(-50%, -20%)';
 })
 
+/**
+ * This function makes the character tiles pop up
+ * from random holes
+ */
 let tiles = document.getElementsByClassName('tile');
 console.log(tiles.length);
 console.log(tiles);
@@ -33,10 +37,18 @@ let popup = tiles[i];
 console.log(popup.innerHTML);
 popup.classList.remove('hide');
 
+/**
+ * This function defines what should happen when the cursor
+ * is clicked on a character tile when it popsup
+ */
 popup.addEventListener('click', function() {
     alert('You whacked me')
 })
 
+/**
+ * This function increments the score by 10 for every successful
+ * character whacked
+ */
 let score = document.getElementsByTagName('span')[0];
 let points = Number(0);
 console.log(score.textContent);
@@ -44,3 +56,9 @@ popup.addEventListener('click', function(){
     points += Number(10);
     score.textContent = points;
 })
+
+let timer;
+timer = setTimeout(function(){
+    popup.classList.add('hide');
+}, 1500)
+
