0234b22ec949c2d8dbb06b8a0a80595448a1c55e
Kaybe111 <keith.byrne01@gmail.com>
2022-09-23 08:05:25 +0000

    Add CSS class of hide to all tiles to set character tiles to display as none, then add JS to remove the display at random

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ const cursor = document.getElementById('cursor');
 /**
  * This function makes the boxing glove move with the cursor
  */
-window.addEventListener('mousemove', function(event) {
+window.addEventListener('mousemove', function (event) {
     cursor.style.top = event.pageY + 'px';
     cursor.style.left = event.pageX + 'px';
 })
@@ -12,7 +12,7 @@ window.addEventListener('mousemove', function(event) {
  * This function makes the glove rotate on mousedown
  * to simulate giving a punch
  */
-window.addEventListener('mousedown', function() {
+window.addEventListener('mousedown', function () {
     cursor.style.transform = 'translate(-50%, -20%) rotate(70deg)';
 })
 
@@ -20,7 +20,19 @@ window.addEventListener('mousedown', function() {
  * This function returns the boxing glove to its
  * rest state
  */
- window.addEventListener('mouseup', function() {
+window.addEventListener('mouseup', function () {
     cursor.style.transform = 'translate(-50%, -20%)';
 })
+
+    let tiles = document.getElementsByClassName('tile');
+console.log(tiles.length);
+console.log(tiles);
+    let i = Math.floor(Math.random() * tiles.length);
+console.log(i);
+    let popup = tiles[i];
+console.log(popup.innerHTML);
+    popup.classList.remove('hide');
+
+   
+    })
 
